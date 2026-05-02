57ebe50fec336c398e9a97f09740af55fd6965d9
kjnoxythdst <kjnoxythdstisss@outlook.com>
2026-05-02 12:43:51 -0700

    E

--- Pathfinder_Project_code/Pathfinder_Project_code.ino
+++ Pathfinder_Project_code/Pathfinder_Project_code.ino
@@ -1,9 +1,9 @@
 // --- PIN DEFINITIONS ---
-// Buttons
-const int B1 = D0;
-const int B2 = D1;
-const int B3 = D2;
-const int B4 = D3;
+// Buttons (Renamed to btn1-btn4 to avoid conflict with Arduino binary macros)
+const int btn1 = D0;
+const int btn2 = D1;
+const int btn3 = D2;
+const int btn4 = D3;
 
 // LEDs
 const int LED1 = D4;
@@ -13,11 +13,10 @@ const int LED4 = D10;
 
 void setup() {
   // Set buttons as inputs with internal pull-up resistors
-  // This keeps the pin HIGH when unpressed.
-  pinMode(B1, INPUT_PULLUP);
-  pinMode(B2, INPUT_PULLUP);
-  pinMode(B3, INPUT_PULLUP);
-  pinMode(B4, INPUT_PULLUP);
+  pinMode(btn1, INPUT_PULLUP);
+  pinMode(btn2, INPUT_PULLUP);
+  pinMode(btn3, INPUT_PULLUP);
+  pinMode(btn4, INPUT_PULLUP);
 
   // Set LEDs as outputs
   pinMode(LED1, OUTPUT);
@@ -28,28 +27,28 @@ void setup() {
 
 void loop() {
   // --- SWITCH 1 ---
-  if (digitalRead(B1) == LOW) {    // If Switch 1 is pressed (reads LOW)
-    digitalWrite(LED1, HIGH);      // Turn LED 1 ON
+  if (digitalRead(btn1) == LOW) {    // If Switch 1 is pressed (reads LOW)
+    digitalWrite(LED1, HIGH);        // Turn LED 1 ON
   } else {
-    digitalWrite(LED1, LOW);       // Otherwise, turn it OFF
+    digitalWrite(LED1, LOW);         // Otherwise, turn it OFF
   }
 
   // --- SWITCH 2 ---
-  if (digitalRead(B2) == LOW) {    
+  if (digitalRead(btn2) == LOW) {    
     digitalWrite(LED2, HIGH);      
   } else {
     digitalWrite(LED2, LOW);       
   }
 
   // --- SWITCH 3 ---
-  if (digitalRead(B3) == LOW) {    
+  if (digitalRead(btn3) == LOW) {    
     digitalWrite(LED3, HIGH);      
   } else {
     digitalWrite(LED3, LOW);       
   }
 
   // --- SWITCH 4 ---
-  if (digitalRead(B4) == LOW) {    
+  if (digitalRead(btn4) == LOW) {    
     digitalWrite(LED4, HIGH);      
   } else {
     digitalWrite(LED4, LOW);       
